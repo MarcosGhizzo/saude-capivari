9f9e3a279d151193d6a8b63766bec2383e07c9e1
Marcos Paulo Madeira Ghizzo <marcospaulomadeiraghizzo@gmail.com>
2025-04-08 22:33:26 -0300

    commit

--- src/main/java/br/com/saudeCapiva/domain/mapper/MedicoMapper.java
+++ src/main/java/br/com/saudeCapiva/domain/mapper/MedicoMapper.java
@@ -1,4 +1,25 @@
 package br.com.saudeCapiva.domain.mapper;
 
+import br.com.saudeCapiva.domain.dto.MedicoDto;
+import br.com.saudeCapiva.domain.entity.Medico;
+
 public class MedicoMapper {
+
+    public static MedicoDto toDto(Medico medico) {
+        return new MedicoDto(medico.getId(), medico.getNome(), medico.getCrm(), medico.getEmail(), medico.getSenha(), medico.getEspecialidade(), medico.getTelefone());
+
+    }
+
+    public static Medico toEntity(MedicoDto dto) {
+        Medico medico = new Medico();
+        medico.setId(dto.getId());
+        medico.setNome(dto.getNome());
+        medico.setCrm(dto.getCrm());
+        medico.setEmail(dto.getEmail());
+        medico.setSenha(dto.getSenha());
+        medico.setEspecialidade(dto.getEspecialidade());
+        medico.setTelefone(dto.getTelefone());
+
+        return medico;
+    }
 }
