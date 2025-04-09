baad1c880488c3af2dd2f6bc3ae80c7fb6479427
Marcos Paulo Madeira Ghizzo <marcospaulomadeiraghizzo@gmail.com>
2025-04-08 21:37:25 -0300

    classe Medico

--- src/main/java/br/com/saudeCapiva/domain/dto/MedicoDto.java
+++ src/main/java/br/com/saudeCapiva/domain/dto/MedicoDto.java
@@ -1,4 +1,21 @@
 package br.com.saudeCapiva.domain.dto;
 
+import br.com.saudeCapiva.domain.Especialidade;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MedicoDto {
+
+    private Long id;
+    private String nome;
+    private String crm;
+    private String email;
+    private String senha;
+    private Especialidade especialidade;
+    private String telefone;
+
 }
